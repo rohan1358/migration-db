ceb2ed153ad43f18b08eaad7c8c67094eb27dc11
rohan1358 <691rohan@gmail.com>
2025-06-20 10:08:34 +0700

    migrate content

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ import testConnection from "./testConnection";
 // migrateCompanyWebPageV2()
 
 // step 3
-migrateAccessV2()
+// migrateAccessV2()
 
 // step 4
-// migrateContentV2()
+migrateContentV2()
